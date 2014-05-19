c2e2bea23800ea9daa0e8a97858b414c787a5368
Mikael Östberg <mikael@inloop.se>
2014-05-19 19:19:55 -0400

    Step 4 - Changed into using express

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 var http = require('http');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello express');
+});
+
+var server = http.createServer(app);
+server.listen(process.env.PORT, process.env.IP);
+
+/*
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Devsum!\n'  + req.url);
 }).listen(process.env.PORT, process.env.IP);
+*/
