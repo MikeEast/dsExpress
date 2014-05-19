54fab6d96fdc27cbbb5ef6c0dd4f9313980e9ab9
Mikael Östberg <mikael@inloop.se>
2014-05-19 19:04:49 -0400

    Step 3 - Updated package.json

--- hello.js
+++
@@ -1,3 +0,0 @@
-module.exports.say = function() {
-    return "hello.js";
-}
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var http = require('http');
-var hello = require("./hello");
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Devsum! This is from ' + hello.say() + ' \n'  + req.url);
+  res.end('Hello Devsum!\n'  + req.url);
 }).listen(process.env.PORT, process.env.IP);
