9b1216854c8c8d5ade1293011d86623421606105
Mikael Östberg <mikael@inloop.se>
2014-05-19 19:00:41 -0400

    Step 2 - Added require('hello')

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
+var hello = require("./hello");
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Devsum!\n' + req.url);
+  res.end('Hello Devsum! This is from ' + hello.say() + ' \n'  + req.url);
 }).listen(process.env.PORT, process.env.IP);
